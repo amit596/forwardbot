1804d48f4b7761ca65bb2fac54a646e5c67cc84c
ak4422920 <ak4422920@gmail.com>
2024-12-30 17:10:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '7251898668').split()]
     BOT_SESSION = environ.get("BOT_SESSION", "AKautoforward_bot") 
 
-    PICS = (environ.get('PICS', 'https://graph.org/file/e223aea8aca83e99162bb.jpg'))
+    PICS = (environ.get('PICS', 'https://envs.sh/oob.jpg'))
     
     DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://forward:forward@cluster0.w6hlw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
