d3268db424ad6f3599c43756c58d6174b7c5b57d
ak4422920 <ak4422920@gmail.com>
2024-12-30 16:40:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,16 +6,16 @@ class Config:
     API_ID = environ.get("API_ID", "")
     API_HASH = environ.get("API_HASH", "")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
-    BOT_SESSION = environ.get("BOT_SESSION", "bot") 
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '7251898668').split()]
+    BOT_SESSION = environ.get("BOT_SESSION", "AKautoforward_bot") 
 
     PICS = (environ.get('PICS', 'https://graph.org/file/e223aea8aca83e99162bb.jpg'))
     
-    DATABASE_URI = environ.get("DATABASE_URI", "")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://forward:forward@cluster0.w6hlw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
     
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") # FORCE SUB channel link 
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002190352334'))
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/akmoviebotz") # FORCE SUB channel link 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")  # FORCE SUB ON - OFF
 
 
