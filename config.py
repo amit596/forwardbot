f2d8228f446020a278445e11f8e43b3b55d5b7b6
amit596 <kaliraman449@gmail.com>
2024-12-31 16:47:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     
     API_ID = environ.get("API_ID", "29171167")
     API_HASH = environ.get("API_HASH", "7ea2149629e445936619f06a3c0dc716")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7389442129:AAFwM37Ygb13WUd7xPugq0U1cNAYTP935YY") 
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '7251898668').split()]
     BOT_SESSION = environ.get("BOT_SESSION", "AKautoforward_bot") 
 
